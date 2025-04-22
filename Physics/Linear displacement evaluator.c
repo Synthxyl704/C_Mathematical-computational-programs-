ed445ad53cf3cb4a7169717f097e874ae2f2b3de
IsoAris -RESURRECTION- <159614599+Synthxyl704@users.noreply.github.com>
2025-04-22 21:43:42 +0530

    Create Linear displacement evaluator.c
    using linear interpolation formula

---
+++ Physics/Linear displacement evaluator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+/*
+
+TD - implement a function that reads analog voltage from a position sensor
+     and converts it to a linear displacement value using the linear interpolation formula
+
+*/
+
+uint64_t main(uint64_t argc, char *argv[]) {
+  printf("\n\n");
+
+  // all example values only, please replace w your og/req ones
+
+  float minVoltRange = 0.0f;          // x1 
+  float maxVoltRange = 10.0f;         // x2 
+  float minPhysicalPos = 0.0f;        // y1 
+  float maxPhysicalPos = 100.0f;      // y2 
+  float sensorVoltReading = 2.5f;     // x aka analog volt
+
+  float voltReadingAvg = (sensorVoltReading - minVoltRange) / (maxVoltRange - minVoltRange);
+  float displacementVal = (maxPhysicalPos - minPhysicalPos);
+
+  float calcPhysicalPos = minPhysicalPos + (voltReadingAvg * displacementVal);
+
+  printf("Linear displacement value: [%.6f]mm\n\n\n", calcPhysicalPos); // just enough for no data truncation
+  return EXIT_SUCCESS;
+}
